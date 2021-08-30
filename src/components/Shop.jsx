6d9a01602a6617a422f1ace777bbc428651f5de4
Evgenii Kolesnikov <jekow@yandex.ru>
2021-08-30 23:57:56 +0300

    Add API  and loading goods

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,7 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import { API_KEY, API_URL } from '../config';
+import GoodsList from './GoodsList';
+import Preloader from './Preloader';
+
 const Shop = () => {
-  return ( 
-    <main className="container content">Hi</main>
-  );
-}
+  const [goods, setGoods] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(function getGoods() {
+    fetch(API_URL, { headers: { Authorization: API_KEY } })
+    .then(response => response.json())
+    .then(data => {
+      data.featured && setGoods(data.featured);
+      setLoading(false);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }, []);
+
+  return (
+    <main className='container content'>
+      {loading ? (<Preloader />) : <GoodsList goods={goods} /> }
+    </main>
+  )
+};
 
 export default Shop;
