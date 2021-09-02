e2d45da2dd7ff5ab4125473edd8f5f6c0c74b0eb
Evgenii Kolesnikov <jekow@yandex.ru>
2021-09-02 22:28:02 +0300

    Change footer text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
             target='_blank'
             rel='noreferrer'
           >
-            More Links
+            Repo
           </a>
         </div>
       </div>
