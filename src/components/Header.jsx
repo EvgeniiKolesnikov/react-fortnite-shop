a7ba8e066b290347ae4f6214a81369c9900f6c49
Evgenii Kolesnikov <jekow@yandex.ru>
2021-09-02 22:26:17 +0300

    Add repo links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,20 @@
 const Header = () => {
-  return ( 
+  return (
     <nav className='blue darken-1'>
-      <div className="nav-wrapper">
-        <a href="#" className="brand-logo">React Fortnite Shop</a>
-        <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li><a href="!#">Repo</a></li>
+      <div className='nav-wrapper'>
+        <a href='.' className='brand-logo'>
+          React Fortnite Shop
+        </a>
+        <ul id='nav-mobile' className='right hide-on-med-and-down'>
+          <li>
+            <a href='https://github.com/EvgeniiKolesnikov/react-fortnite-shop' target='_blank' rel='noreferrer'>
+              Repo
+            </a>
+          </li>
         </ul>
       </div>
     </nav>
   );
-}
+};
 
 export default Header;
